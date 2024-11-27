8f9d62836ec4e80585f17c48aa89579f30b04b03
catieexe <pananona10@gmail.com>
2024-11-27 19:13:57 +0000

    commented 2nd obj loader

--- SetupOpenGL/ObjLoader.h
+++ SetupOpenGL/ObjLoader.h
@@ -8,7 +8,7 @@ class ObjLoader
 
 	public:
 	ObjLoader();
-	~ObjLoader();
+	//~ObjLoader();
 	bool load(const char* filename, std::vector<glm::vec4>& vertices, std::vector<glm::vec3>& normals, std::vector<unsigned short>& elements);
 	
 };
